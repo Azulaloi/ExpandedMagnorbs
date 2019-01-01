914bff0b7482ee8ca2fcc10ca15c12fcce7cbb41
Azulaloi Champagne <azulaloichampagne@gmail.com>
2019-01-01 17:36:52 -0500

    Phase Shift

--- effects/phaseshift/phaseshift.lua
+++ effects/phaseshift/phaseshift.lua
@@ -4,8 +4,9 @@ require "/scripts/status.lua"
 function init()
   animator.setParticleEmitterOffsetRegion("sparkles", mcontroller.boundBox())
   animator.setParticleEmitterActive("sparkles", config.getParameter("particles", true))
-  effect.setParentDirectives("fade=FFFFCC;0.03?border=2;FFFFCC20;00000000")
-
+  --effect.setParentDirectives("fade=FFFFCC;0.03?border=2;FFFFCC20;00000000")
+  effect.setParentDirectives("?saturation=-80?multiply=B890DB20")
+  --8A25DD
   effect.addStatModifierGroup({{stat = "invulnerable", amount = 1}})
 end
 
@@ -13,5 +14,5 @@ function update(dt)
 end
 
 function uninit()
-
+	status.expire()
 end
